56d9704ee31f673ed2b73a240e6dcd473eccfbbb
syery <9673yuki@gmail.com>
2019-03-16 15:24:57 +0900

    センサーリスト表示

--- app/src/main/java/com/example/sensorapp01/MainActivity.java
+++ app/src/main/java/com/example/sensorapp01/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.sensorapp01;
 
+import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
@@ -12,13 +17,19 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        //端末センサーリストを表示
+        SensorManager sm = (SensorManager)this.getSystemService(Context.SENSOR_SERVICE);
+        List<Sensor> sensors = sm.getSensorList(Sensor.TYPE_ALL);
+        for (Sensor s: sensors){
+            Log.d("SENSOR","名前は" + s.getName());
+        }
+    }
     @Override
     public void onSensorChanged(SensorEvent sev){
 
     }
-    
+
     @Override
     public void onAccuracyChanged(Sensor s, int ac){
 
