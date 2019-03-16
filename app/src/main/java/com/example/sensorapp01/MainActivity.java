90a58da84c9146c0c34fc31ce6b4a1f410c65b04
syery <9673yuki@gmail.com>
2019-03-16 14:57:20 +0900

    センサーイベントリスナーの導入

--- app/src/main/java/com/example/sensorapp01/MainActivity.java
+++ app/src/main/java/com/example/sensorapp01/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.sensorapp01;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public void onSensorChanged(SensorEvent sev){
+
+    }
+    
+    @Override
+    public void onAccuracyChanged(Sensor s, int ac){
+
+    }
+
+
 }
